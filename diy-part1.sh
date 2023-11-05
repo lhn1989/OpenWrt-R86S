eb35e4777d27dbbc6152da9fc7ac588452bff94e
lhn1989 <lhn1989@users.noreply.github.com>
2023-11-05 21:47:37 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -32,7 +32,7 @@ set -x
 
 # Add SSR
 sed -i "/helloworld/d" "feeds.conf.default"
-echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
+echo "src-git helloworld https://github.com/fw876/helloworld.git;main" >> "feeds.conf.default"
 
 # Add cussubconverter
 echo "src-git cussubconverter https://github.com/lhn1989/luci-app-cussubconverter.git" >> "feeds.conf.default"
